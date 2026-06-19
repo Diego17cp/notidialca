d75182ac58fbb62138e73492deba025127e8aa9a
Diego17 <diegoalbertocastropastor@gmail.com>
2026-06-18 20:57:10 -0500

    add riverpod providers for flutter secure storage and shared preferences

---
+++ lib/core/storage/providers/flutter_secure_storage_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'flutter_secure_storage_provider.g.dart';
+
+@riverpod
+FlutterSecureStorage flutterSecureStorage(Ref ref) =>
+    const FlutterSecureStorage(
+      aOptions: AndroidOptions(encryptedSharedPreferences: true),
+    );
---
+++ lib/core/storage/providers/flutter_secure_storage_provider.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'flutter_secure_storage_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$flutterSecureStorageHash() =>
+    r'24f4375aee0d1441c71de6d342151ed125df793d';
+
+/// See also [flutterSecureStorage].
+@ProviderFor(flutterSecureStorage)
+final flutterSecureStorageProvider =
+    AutoDisposeProvider<FlutterSecureStorage>.internal(
+      flutterSecureStorage,
+      name: r'flutterSecureStorageProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$flutterSecureStorageHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FlutterSecureStorageRef = AutoDisposeProviderRef<FlutterSecureStorage>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
