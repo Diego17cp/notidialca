2da6a01917c320c3fb094a1618fb357dffa482cc
Diego17 <diegoalbertocastropastor@gmail.com>
2026-06-24 21:58:59 -0500

    implement Google Fonts Onest text theme and enable Material 3 for dark mode

--- lib/app/theme/theme.dart
+++ lib/app/theme/theme.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:notidialca/app/theme/color_schemes.dart';
 
 class AppTheme {
   static ThemeData light = ThemeData(
     useMaterial3: true,
     colorScheme: lightColorScheme,
+    textTheme: GoogleFonts.onestTextTheme(ThemeData.light().textTheme),
   );
+
   static ThemeData dark = ThemeData(
+    useMaterial3: true,
     colorScheme: darkColorScheme,
+    textTheme: GoogleFonts.onestTextTheme(ThemeData.dark().textTheme),
   );
 }
